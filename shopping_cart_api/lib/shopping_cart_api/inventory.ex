4ff5f20d704e6389e342c23964f2cfaadfd05283
Ajaz Ur Rehman <ajazurrehman4@gmail.com>
2024-07-20 12:38:11 +0000

    Create inventory model

---
+++ shopping_cart_api/lib/shopping_cart_api/inventory.ex
@@ -0,0 +1,41 @@
+defmodule ShoppingCartApi.Inventory do
+  alias ShoppingCartApi.Repo
+  alias ShoppingCartApi.Product
+
+  def list_products do
+    Repo.all(Product)
+  end
+
+  def get_product!(id), do: Repo.get!(Product, id)
+
+  def create_product(attrs \\ %{}) do
+    %Product{}
+    |> Product.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_product(%Product{} = product, attrs) do
+    product
+    |> Product.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_product(%Product{} = product) do
+    Repo.delete(product)
+  end
+
+  def confirm_purchase(items) do
+    Repo.transaction(fn ->
+      Enum.each(items, fn %{"id" => id, "quantity" => quantity} ->
+        product = get_product!(id)
+        new_quantity = product.quantity - quantity
+
+        if new_quantity < 0 do
+          Repo.rollback({:error, "Not enough inventory for product #{product.name}"})
+        else
+          update_product(product, %{quantity: new_quantity})
+        end
+      end)
+    end)
+  end
+end
