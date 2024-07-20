90e63570550829ede9a3485693f7d05377607fb6
Ajaz Ur Rehman <ajazurrehman4@gmail.com>
2024-07-20 15:56:55 +0000

    Update test db credentials

--- shopping_cart_api/config/test.exs
+++ shopping_cart_api/config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :shopping_cart_api, ShoppingCartApi.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "admin",
+  password: "password",
   hostname: "localhost",
   database: "shopping_cart_api_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
