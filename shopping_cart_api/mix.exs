a00ae976f4b12afca5b873b55d1399b746a1fca2
Ajaz Ur Rehman <ajazurrehman4@gmail.com>
2024-07-21 10:55:50 +0000

    Add cors package to api

--- shopping_cart_api/mix.exs
+++ shopping_cart_api/mix.exs
@@ -36,6 +36,7 @@ defmodule ShoppingCartApi.MixProject do
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
+      {:cors_plug, "~> 3.0"},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:swoosh, "~> 1.5"},
       {:finch, "~> 0.13"},
