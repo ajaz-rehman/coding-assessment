a6009e1c64f8b01db85d11938f6895c444f46cf3
Ajaz Ur Rehman <ajazurrehman4@gmail.com>
2024-07-21 10:18:29 +0000

    Add more validations

--- shopping_cart_api/lib/shopping_cart_api/product.ex
+++ shopping_cart_api/lib/shopping_cart_api/product.ex
@@ -16,5 +16,7 @@ defmodule ShoppingCartApi.Product do
     product
     |> cast(attrs, [:name, :description, :price, :quantity])
     |> validate_required([:name, :description, :price, :quantity])
+    |> validate_number(:price, greater_than: 0)
+    |> validate_number(:quantity, greater_than_or_equal_to: 0)
   end
 end
