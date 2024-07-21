53e8974c73eb5cea8cb0fea093a7a9d5f38d9aa7
Ajaz Ur Rehman <ajazurrehman4@gmail.com>
2024-07-21 12:21:06 +0000

    Update button component

--- shopping_cart_vue/src/components/atoms/Button.vue
+++ shopping_cart_vue/src/components/atoms/Button.vue
@@ -17,6 +17,11 @@ export default defineComponent({
 		variant: {
 			type: String,
 			default: "blue",
+			validator: (value: string) => ["blue", "green", "red"].includes(value),
+		},
+		outlined: {
+			type: Boolean,
+			default: false,
 		},
 		disabled: {
 			type: Boolean,
@@ -25,8 +30,19 @@ export default defineComponent({
 	},
 	computed: {
 		buttonClasses() {
+			if (this.outlined) {
+				return {
+					"btn rounded px-3 py-1": true,
+					"border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white": this.variant === "blue",
+					"border border-green-500 text-green-500 hover:bg-green-500 hover:text-white":
+						this.variant === "green",
+					"border border-red-500 text-red-500 hover:bg-red-500 hover:text-white": this.variant === "red",
+					"cursor-not-allowed opacity-50": this.disabled,
+				};
+			}
+
 			return {
-				"btn rounded px-4 py-2": true,
+				"btn rounded px-3 py-1": true,
 				"bg-blue-500 text-white hover:bg-blue-600": this.variant === "blue",
 				"bg-green-500 text-white hover:bg-green-600": this.variant === "green",
 				"bg-red-500 text-white hover:bg-red-600": this.variant === "red",
