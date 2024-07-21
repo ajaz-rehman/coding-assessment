1e39712845d392b095cc721fc184c1d943f62dc1
Ajaz Ur Rehman <ajazurrehman4@gmail.com>
2024-07-21 17:09:12 +0000

    Use injected function for event

--- shopping_cart_vue/src/components/templates/ShoppingCartSection.vue
+++ shopping_cart_vue/src/components/templates/ShoppingCartSection.vue
@@ -5,14 +5,14 @@
 			<ShoppingCartItemList :items="items" />
 			<div class="flex gap-2 items-center justify-between sm:justify-end">
 				<p class="font-bold">Total: ${{ total }}</p>
-				<Button @click="onPurchase" variant="green">Purchase</Button>
+				<Button @click="onPurchaseItems" variant="green">Purchase</Button>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script lang="ts">
-import { computed } from "vue";
+import { computed, inject } from "vue";
 import { CartItem } from "../../types";
 import Button from "@/components/atoms/Button.vue";
 import ShoppingCartItemList from "@/components/organisms/ShoppingCartItemList.vue";
@@ -25,23 +25,26 @@ export default {
 			required: true,
 		},
 	},
-	emits: ["purchase"],
 	components: {
 		Button,
 		ShoppingCartItemList,
 	},
-	setup(props, { emit }) {
+	setup(props) {
 		const total = computed(() => {
 			return props.items.reduce((sum, item) => sum + item.price * item.quantity, 0).toFixed(2);
 		});
 
-		const onPurchase = () => {
-			emit("purchase");
+		const purchaseItems = inject<Function>("purchaseItems");
+
+		const onPurchaseItems = () => {
+			if (purchaseItems) {
+				purchaseItems(props.items);
+			}
 		};
 
 		return {
 			total,
-			onPurchase,
+			onPurchaseItems,
 		};
 	},
 };
